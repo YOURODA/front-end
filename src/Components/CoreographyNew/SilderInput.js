efffb8580e40d55a60019d237260a96fb7ae1406
okanserbest <31311498+okanserbest@users.noreply.github.com>
2021-02-12 18:15:49 +0300

    Update SilderInput.js

--- src/Components/CoreographyNew/SilderInput.js
+++ src/Components/CoreographyNew/SilderInput.js
@@ -1,7 +1,7 @@
 import React, { useState, useCallback } from "react";
 import * as actionTypes from "../../store/actions/actionTypes";
 import { connect } from "react-redux";
-import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
@@ -11,7 +11,7 @@ const useStyles = makeStyles(() => ({
   root: {
     width: 200,
   },
-})); FormData
+}));
 
 const marksX = [
   {
@@ -59,11 +59,6 @@ const marksY = [
 ];
 
 function SilderInput(props) {
-  const backgroundColor =
-    props.robot === "L"
-      ? `rgb(${lColor1}, ${lColor2}, ${lColor3})`
-      : `rgb(${rColor1}, ${rColor2}, ${rColor3})`;
-
   const { rColor1, rColor2, rColor3, lColor1, lColor2, lColor3 } = props.colour;
   const classes = useStyles();
   const [valueY, setValueY] = useState(0);
@@ -76,6 +71,11 @@ function SilderInput(props) {
     debounce((e) => updateReduxY(e), 1000),
     []
   );
+  const backgroundColor =
+    props.robot === "L"
+      ? `rgb(${lColor1}, ${lColor2}, ${lColor3})`
+      : `rgb(${rColor1}, ${rColor2}, ${rColor3})`;
+
   const updateReduxX = (event) => {
     const { robot } = props;
     console.log("updateReduxX", event);
@@ -103,38 +103,36 @@ function SilderInput(props) {
       <Typography id="continuous-slider" gutterBottom>
         {props.label}
       </Typography>
+      
+        
+          <Slider
+            min={0}
+            max={255}
+            track={true}
+            defaultValue={valueY}
+            marks={marksY}
+            step={1}
+            scale={(x) => x}
+            // orientation="vertical"
+            onChange={(e, newValue) => {
+              setValueY(newValue);
+              debounceReduxY(newValue);
+            }}
+          />
 
-      <Slider
-        color={backgroundColor}
-        min={0}
-        max={255}
-        track={true}
-        defaultValue={valueY}
-        marks={marksY}
-        step={1}
-        scale={(x) => x}
-        orientation="vertical"
-        onChange={(e, newValue) => {
-          setValueY(newValue);
-          debounceReduxY(newValue);
-        }}
-      />
-
-      <Slider
-      color={backgroundColor}
-        backgroundColor={backgroundColor}
-        min={0}
-        max={255}
-        track={true}
-        defaultValue={valueX}
-        marks={marksX}
-        step={1}
-        scale={(x) => x}
-        onChange={(e, newValue) => {
-          setValueX(newValue);
-          debounceReduxX(newValue);
-        }}
-      />
+        <Slider
+          min={0}
+          max={255}
+          track={true}
+          defaultValue={valueX}
+          marks={marksX}
+          step={1}
+          scale={(x) => x}
+          onChange={(e, newValue) => {
+            setValueX(newValue);
+            debounceReduxX(newValue);
+          }}
+        />
     </div>
   );
 }
@@ -156,4 +154,4 @@ const mapDispatchToProps = (dispatch) => {
       dispatch({ type: actionTypes.RIGHT_VER_VALUE, rightVerValue }),
   };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(useStyles)(SilderInput));
+export default connect(mapStateToProps, mapDispatchToProps)(SilderInput);
