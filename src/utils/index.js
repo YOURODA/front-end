93f59a2cc59ac3dbda7bde554883988357f7bff1
Eray Eroglu <Eray.Eroglu@d-teknoloji.com.tr>
2021-12-01 19:23:07 +0300

    developed about robotModel

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,16 @@ export const milisToMinutesAndSeconds = (mil) => {
   let seconds = ((mil % 60000) / 1000).toFixed(0);
   return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 };
-export const regulatorCorTry = ({ cor, smoke }) => {
+
+const regulatorRobotModel=(level)=>{
+  if(level <134){
+    return Math.floor((level*134)/255);
+  }
+  else if (level> 134 && level <240 ){
+    return Math.floor((level*106)/255)
+  }
+}
+export const regulatorCorTry = ({ cor, smoke, robotMoodel }) => {
 
   const {
     robot: {
@@ -20,13 +29,31 @@ export const regulatorCorTry = ({ cor, smoke }) => {
       colour: { lColor1, lColor2, lColor3, rColor1, rColor2, rColor3 },
     },
   } = cor;
-  return `${LHor ? LHor : "0"},0,${LVer ? LVer : "0"},0,${LSpeed ? LSpeed : "0"},${
+  let LNewBlinker=0;
+  let LNewBrightness=0;
+  let RNewBlinker=0;
+  let RNewBrightness=0;
+
+  let result= `${LHor ? LHor : "0"},0,${LVer ? LVer : "0"},0,${LSpeed ? LSpeed : "0"},${
     LBrightness ? LBrightness : "0"
   },${lColor1},${lColor2},${lColor3},0,${LBlinker ? LBlinker : "0"},0,0,${
     RHor ? RHor : "0"
   },0,${RVer ? RVer : "0"},0,${RSpeed ? RSpeed : "0"},${
     RBrightness ? RBrightness : "0"
   },${rColor1},${rColor2},${rColor3},0,${RBlinker ? RBlinker : "0"},0,0`;
+
+  if  (robotMoodel=== "14chw"){
+    result= `${LHor ? LHor : "0"},0,${LVer ? LVer : "0"},0,${LSpeed ? LSpeed : "0"},${
+      LBrightness ? LBrightness : "0"
+    },${lColor1},${lColor2},${lColor3},0,${LBlinker ? LBlinker : "0"},0,0,${
+      RHor ? RHor : "0"
+    },0,${RVer ? RVer : "0"},0,${RSpeed ? RSpeed : "0"},${
+      RBrightness ? RBrightness : "0"
+    },${rColor1},${rColor2},${rColor3},0,${RBlinker ? RBlinker : "0"},0,0`;
+  }
+
+
+  return result;
 };
 
 
