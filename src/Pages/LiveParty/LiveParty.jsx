3ee6601e17af98018e566ae2ed8ffcdf5519afab
okanserbest <31311498+okanserbest@users.noreply.github.com>
2022-06-05 19:21:41 +0300

    feat: routes

---
+++ src/Pages/LiveParty/LiveParty.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+/*
+*Live parti için ana sayfa
+*/
+const LiveParty=(props)=> {
+  return (
+    <div>LiveParty</div>
+  )
+}
+
+LiveParty.propTypes = {}
+
+export default LiveParty
