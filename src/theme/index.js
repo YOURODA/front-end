80138ccb9741dd76c644d70c8cd501990bbd7381
okanserbest <31311498+okanserbest@users.noreply.github.com>
2022-10-08 18:03:17 +0300

    fiexxed: env deneme

--- src/theme/index.js
+++ src/theme/index.js
@@ -2,6 +2,7 @@ import { createMuiTheme } from "@material-ui/core";
 
 import palette from "./palette";
 import typography from "./typography";
+
 import overrides from "./overrides";
 
 const theme = createMuiTheme({
